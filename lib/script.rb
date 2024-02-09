159f6085fc7ec5196481b08879bd094f2f958e3d
Methal Abeywickrama <methal.abeywickrama@icloud.com>
2024-02-09 13:23:27 +0530

    implement a method for adding a piece to a column
    -columns variable updated after each change to the board variable as its
    otherwise not reflected

--- lib/script.rb
+++ lib/script.rb
@@ -2,19 +2,28 @@
 
 # rubocop: convention:Style/StringLiterals
 
+require_relative 'errors'
+
 class ConnectFour
-  attr_accessor :blank, :white, :black
+  attr_accessor :board, :columns
+  WHITE = "\u26AA".freeze
+  BLACK = "\u26AB".freeze
+  BLANK = "\u274D ".freeze
 
   def initialize
-    @white = "\u26AA"
-    @black = "\u26AB"
-    @blank = "\u274D "
-    @board = Array.new(6, Array.new(7, @blank))
+    @board = Array.new(6) { Array.new(7, BLANK) }
     # @board.push([@white, @blank, @black, @white, @blank, @black, @blank])
     @board.push(["1 ", "2 ", "3 ", "4 ", "5 ", "6 ", "7 "])
     @columns = column_set
     p @columns
     p @board
+  end
+
+  def change_square(a, b)
+    p @board
+    p @board[a]
+    p @board[a][b]
+    @board[a][b] = WHITE
   end
 
   def define_column(column_no)
@@ -36,8 +45,29 @@ class ConnectFour
     columns
   end
 
-  def add_piece(column_no, color)
+  def check_win(shape)
+    won = false
+    @columns.each do |column|
+      case column
+      in [^shape, ^shape, ^shape, ^shape]
+      end
+    end
+  end
+
+  def add_piece(column_no=2, color='shite')
     column = @columns[column_no - 1]
+    color == 'white' ? color = WHITE : color = BLACK
+    raise ColumnFullError.new('sd') unless column.any?(BLANK)
+
+    column.each_with_index do |space, index|
+
+      
+      if column[index + 1] != BLANK && space == BLANK
+        @board[index][column_no - 1] = color
+        @columns = column_set
+      end
+   
+    end
     
   end
 
@@ -55,11 +85,23 @@ def play_game
   puts 'lets play a game of connect four'
   connect_four = ConnectFour.new
   connect_four.print_board
-  won = false
 
-  while !won
-    'Player 1, select a column'
+  won = false
+  begin
+    connect_four.add_piece(2, 'white')
+    connect_four.add_piece(2, 'white')
+    connect_four.add_piece(2, 'white')
+    connect_four.add_piece(2, 'white')
+    connect_four.add_piece(2, 'white')
+    connect_four.add_piece(2, 'white')
+    connect_four.print_board
+  rescue ColumnFullError
+    puts 'its ok'
   end
+
+  # while !won
+  #   'Player 1, select a column'
+  # end
 
 end
 
